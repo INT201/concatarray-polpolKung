165f6fe68da568242e5b219c1f758b1b3c200088
polpolKung <chanpol.thip@kmutt.ac.th>
2022-09-10 19:46:10 +0700

    test2

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,6 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  return array1.concatArray(array2)
+  return array1.concat(array2)
 }
 module.exports = concatArray
