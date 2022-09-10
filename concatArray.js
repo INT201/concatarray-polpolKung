9757602558f329a728095f70ff530fe6a30fc395
polpolKung <chanpol.thip@kmutt.ac.th>
2022-09-10 20:21:39 +0700

    output5

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,12 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  return array1?.concat(array2)
+  if(!array1) {
+      return array2
+  } else if(!array2) {
+      return array1
+  } else {
+      return array1.concat(array2)
+  }
 }
 module.exports = concatArray
